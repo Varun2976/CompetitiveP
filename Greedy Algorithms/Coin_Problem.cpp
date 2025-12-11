1305811ba59620a1e9c703604984130b07e99fe9
Varun2976 <vsnair2976@gmail.com>
2025-12-11 21:00:28 +0530

    Greedy updated and dp added

--- Greedy Algorithms/Coin_Problem.cpp
+++ Greedy Algorithms/Coin_Problem.cpp
@@ -1,33 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-/*
--------------------------- COIN PROBLEM ---------------------------
-    - Certain number of coins are given and you are to make the 'n' sum of money using those coins
-    - It should be efficient
-
-*/
-
-int coin_problem(int amount , vector<int>& coins){
-    const int INF = 1e9;
-    vector<int> dp(amount + 1 ,INF);
-
-    dp[0] = 0;
-
-    for(int i =0 ; i <= amount; i++){
-        for(int coin : coins){
-            if(i - coin >= 0){
-                dp[i] = min(dp[i] , 1 + dp[i - coin]);
-            }
+void coin(int amount , vector<int> &coins){
+    sort(coins.rbegin() , coins.rend());
+    int count = 0;
+    for(int c : coins){
+        while(amount >= c){
+            amount -= c;
+            count++;
         }
     }
-    return dp[amount];
+    cout << count << endl;
+    
 }
 int main(){
-    vector <int> coins = {1,3,4};
-    int amount;
-    cout << "Enter the amount you wanna calculate : ";
-    cin >> amount ;
+    vector<int>coins = {1,3,4};
+    int amount = 6;
 
-    cout << coin_problem(amount , coins) << endl;
+    coin(amount , coins);
+
     return 0;
 }
