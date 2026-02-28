d1d7ad6989ce3a83cf8ed6f10aab5d4e6efb27ea
Varun2976 <vsnair2976@gmail.com>
2026-03-01 03:46:53 +0530

    LIS

--- Dynamic Programming/LIS.cpp
+++ Dynamic Programming/LIS.cpp
@@ -1,11 +1,39 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-long long LIS(int m , int n){
-    long long res = 1;
-    int k = min(m-1,n-1);
-    for(int i = 0 ; i < )
+void LIS(vector<int>& v, int n) {
+
+    vector<int> length(n);
+
+    for (int k = 0; k < n; k++) {
+        length[k] = 1;
+
+        for (int i = 0; i < k; i++) {
+            if (v[i] < v[k]) {
+                length[k] = max(length[k], length[i] + 1);
+            }
+        }
+    }
+
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        ans = max(ans, length[i]);
+    }
+
+    cout << ans << endl;
 }
-int main(){
-    
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> V(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> V[i];
+    }
+
+    LIS(V, n);
+
     return 0;
 }
